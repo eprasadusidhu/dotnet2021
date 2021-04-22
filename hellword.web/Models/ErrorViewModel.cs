b19da6eb058f5f14151938dbff099fdbe476983b
eprasadusidhu <elaprolusidhu@gmail.com>
2021-04-22 21:41:22 +0530

    first commit

---
+++ hellword.web/Models/ErrorViewModel.cs
@@ -0,0 +1,11 @@
+using System;
+
+namespace hellword.web.Models
+{
+    public class ErrorViewModel
+    {
+        public string RequestId { get; set; }
+
+        public bool ShowRequestId => !string.IsNullOrEmpty(RequestId);
+    }
+}
